c9c109775128a3b0688bd941974916af33e43d22
Aleksey Derkach <aleksey.derkach@gmail.com>
2016-08-21 15:11:29 +0300

    Retrieve an XML file with image data

--- kitlvdownloader.py
+++ kitlvdownloader.py
@@ -4,11 +4,20 @@
 import sys, argparse, requests
 from pyquery import PyQuery
 
+BASE_DATA_URL = "http://media-kitlv.nl/index.php?option=com_memorixbeeld&view=record&format=topviewxml&tstart=0&id="
+
 def get_image_datafile_id(page_url):
 	r = requests.get(page_url)
 	pq = PyQuery(r.content)
 	datafile_id = pq("div.detailresult").attr("id")
 	return datafile_id[3:]
+
+def get_image_datafile(datafile_id):
+	global BASE_DATA_URL
+	xml_url = BASE_DATA_URL + datafile_id
+	r = requests.get(xml_url)
+	xml = r.content
+	return xml
 
 def main(argv):
 	parser = argparse.ArgumentParser()
@@ -18,6 +27,7 @@ def main(argv):
 	page_url = results.url
 
 	datafile_id = get_image_datafile_id(page_url)
+	xml = get_image_datafile(datafile_id)
 
 if __name__ == '__main__':
 	main(sys.argv[1:])
